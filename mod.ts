3c65d411d6b34c987d2144560bb7f1644cc28cc6
AmteurPotion <1k0j2i4@gmail.com>
2022-11-06 06:40:43 +0900

    unused var

--- mod.ts
+++ mod.ts
@@ -3,6 +3,7 @@ import { runCommand } from "dcra/mod.ts";
 //import * as init from "./init/cmd.ts";
 //import * as build from "./build/cmd.ts";
 
+// deno-lint-ignore no-unused-vars
 const { options } = await new cli.Command()
   .name("deno-create-react-app")
   .version("0.1.2")
