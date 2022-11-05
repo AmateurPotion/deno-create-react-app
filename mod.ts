18b64e6e322a1d23d1de74626b290478bfdaba7b
AmteurPotion <1k0j2i4@gmail.com>
2022-11-06 07:48:33 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,12 +1,12 @@
 import {cli} from "./deps.ts";
-import { runCommand } from "dcra/mod.ts";
-//import * as init from "./init/cmd.ts";
-//import * as build from "./build/cmd.ts";
+import { runCommand, initCommand, buildCommand } from "dcra/mod.ts";
 
 // deno-lint-ignore no-unused-vars
 const { options } = await new cli.Command()
   .name("deno-create-react-app")
   .version("0.1.2")
   .description("Create React App with Deno")
+  .command("init", initCommand)
   .command("run", runCommand)
+  .command("build", buildCommand)
   .parse(Deno.args);
