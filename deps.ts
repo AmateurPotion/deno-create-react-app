7f75e88bf3e1cf8e332f8272cd98ffe81bf41e59
AmteurPotion <1k0j2i4@gmail.com>
2022-11-06 07:55:19 +0900

    Delete deps.ts

--- deps.ts
+++
@@ -1,9 +0,0 @@
-export * as cli from "https://deno.land/x/cliffy@v0.25.4/command/mod.ts";
-export * as oak from "https://deno.land/x/oak@v11.1.0/mod.ts";
-
-export * as fs from "https://deno.land/std@0.138.0/fs/mod.ts";
-export * as path from 'https://deno.land/std@0.138.0/path/mod.ts';
-export * as server from "https://deno.land/std@0.138.0/http/server.ts";
-export * as datetime from "https://deno.land/std@0.138.0/datetime/mod.ts";
-
-export { open } from "https://deno.land/x/opener@v1.0.1/mod.ts";
